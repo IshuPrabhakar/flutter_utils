623946a5539f438b5eeada52fb21d3c923d4eb15
Ishu Prabhakar <72759521+IshuPrabhakar@users.noreply.github.com>
2024-02-15 16:56:00 +0530

    adds part of directive

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,11 @@
 library utils;
 
 import 'package:animations/animations.dart';
-export 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
+export 'package:animations/animations.dart';
+
+part './animations/navigator_animations.dart';
 part './enum/border_radius.dart';
 part './widgets/animated_button.dart';
-part './animations/navigator_animations.dart';
+part 'widgets/shimmer.dart';
--- lib/utils/widgets/shimmer.dart
+++ lib/utils/widgets/shimmer.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+part of utils;
 
 const shimmerGradient = LinearGradient(
   colors: [
