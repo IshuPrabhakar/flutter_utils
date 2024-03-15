e958e8f463adf53474b6cc6d1e4b3172a86a2ed0
Ishu Prabhakar <IshuPrabhakar3679@outlook.com>
2024-03-15 11:34:14 +0530

    adds defaultdatetimeformat extension

--- lib/extension/datetime_extension.dart
+++ lib/extension/datetime_extension.dart
@@ -27,4 +27,80 @@ extension DateHelper on DateTime {
   /// Returns a string formatted date
   ///  Example: 15:08 PM
   String get time24Hrs => DateFormat.Hm().toString();
+
+  String get defaultDateTimeFormat {
+    DateTime today = DateTime.now();
+    Duration oneDay = const Duration(days: 1);
+    Duration twoDay = const Duration(days: 2);
+    Duration oneWeek = const Duration(days: 7);
+    late String month;
+
+    switch (this.month) {
+      case 1:
+        month = "January";
+        break;
+      case 2:
+        month = "February";
+        break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+      case 6:
+        month = "June";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case 8:
+        month = "August";
+        break;
+      case 9:
+        month = "September";
+        break;
+      case 10:
+        month = "October";
+        break;
+      case 11:
+        month = "November";
+        break;
+      case 12:
+        month = "December";
+        break;
+    }
+
+    Duration difference = today.difference(this);
+    var formattedTime = DateFormat.jm().format(this);
+    if (difference.compareTo(oneDay) < 1) {
+      return "$formattedTime, Today";
+    } else if (difference.compareTo(twoDay) < 1) {
+      return "$formattedTime, Yesterday";
+    } else if (difference.compareTo(oneWeek) < 1) {
+      switch (weekday) {
+        case 1:
+          return "$formattedTime, Monday";
+        case 2:
+          return "$formattedTime, Tuesday";
+        case 3:
+          return "$formattedTime, Wednesday";
+        case 4:
+          return "$formattedTime, Thursday";
+        case 5:
+          return "$formattedTime, Friday";
+        case 6:
+          return "$formattedTime, Saturday";
+        default:
+          return "$formattedTime, Sunday";
+      }
+    } else if (year == today.year) {
+      return '$day $month $year';
+    } else {
+      return '$day $month $year';
+    }
+  }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,15 +3,11 @@ library utils;
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'dart:math' as math;
-import 'dart:ui';
 
-import 'widgets/expandable_fab/action_button_builder.dart';
-
+import 'package:flutter/foundation.dart';
 export 'package:animations/animations.dart';
 
 part './animations/navigator_animations.dart';
 part './enum/border_radius.dart';
 part './widgets/animated_button.dart';
 part 'widgets/shimmer.dart';
-part './widgets/expandable_fab/expandable_fab.dart';
